e0168b276bf43c36555e827bc451425a99961a5b
ehgkj <diogo.sales.ext@bayer.com>
2023-08-28 18:21:11 -0300

    feat: Static routes created

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use std::net::SocketAddr;
 use axum::Router;
 use axum::extract::{Query, Path};
 use axum::response::{Html, IntoResponse};
-use axum::routing::get;
+use axum::routing::{get, get_service};
 use serde::Deserialize;
+use tower_http::services::ServeDir;
 
 #[derive(Debug, Deserialize)]
 struct HelloParams {
@@ -14,14 +15,14 @@ struct HelloParams {
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route("/hello", get(handler_hello))
-        .route("/hello2/:name", get(handler_hello2));
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_statics());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
 }
@@ -37,4 +38,14 @@ async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
 
     Html(format!("Hello2 <strong>{name}</strong>"))
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
+
+fn routes_statics() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -7,5 +7,8 @@ async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
     hc.do_get("/hello2/Mike").await?.print().await?;
+
+    hc.do_get("/src/main.rs").await?.print().await?;
+
     Ok(())
 }
